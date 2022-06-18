1491b77dd2dc12a5ad7066e3cdd50782ea3cc68f
Mike <michalbaz@poczta.onet.pl>
2022-06-18 22:08:14 +0200

    solution working

--- main_graph.v
+++ main_graph.v
@@ -33,8 +33,15 @@ parameter[15:0] t_delay_0_2 = t_delay_default - 200;     //t_del - 0.2s
 parameter[15:0] t_delay_0_8 = t_delay_default * 0.8;     //t_del * 0.8
 parameter[15:0] t_delay_1_2 = t_delay_default * 1.2;		//t_del * 1.2
 
+reg diff_is_positive = 0;
+reg [63:0] millis = 0;
+reg [63:0] t1 = 0;
+reg [63:0] t2 = 0;
+reg [63:0] t_diff = 0;
+reg [15:0] t_delay = t_delay_default;
 reg [2:0] state = 0;
 reg [15:0] tim;
+reg t_diff_state = 0;
 wire [3:0] oSWITCH;
 wire t_out;
 
@@ -47,9 +54,44 @@ timer time_base(
 
 debouncer d(
 	.CLK(CLK), 
-	iSWITCH(SWITCH), 
-	oSWITCH(oSWITCH)
+	.iSWITCH(SWITCH), 
+	.oSWITCH(oSWITCH)
 );
+
+always @(oSWITCH[1]) begin
+	t1 = millis;
+end
+
+always @(oSWITCH[2]) begin
+	t2 = millis;
+end
+
+always @(t_diff) begin
+	if(diff_is_positive && t_diff <= 1000) begin
+		t_delay = t_delay_default;
+	end
+	else begin
+		case(t_diff_state)
+			0: begin
+				if(diff_is_positive) begin
+					t_delay = t_delay_0_3;
+				end
+				else begin
+					t_delay = t_delay_0_2;
+				end
+			end
+			
+			1: begin
+				if(diff_is_positive) begin
+					t_delay = t_delay_0_8;
+				end
+				else begin
+					t_delay = t_delay_1_2;
+				end
+			end
+		endcase
+	end
+end
 
 always @(posedge t_out) begin
 	case(state)
@@ -57,7 +99,7 @@ always @(posedge t_out) begin
 			LED = 4'b0000;
 			if(oSWITCH[3] == 0) begin
 				state = 1;
-				tim = 10;//t_delay_default;
+				tim = t_delay;
 			end
 		end
 		
@@ -72,7 +114,7 @@ always @(posedge t_out) begin
 			LED = 4'b0011;
 			if(oSWITCH[1] == 0 || oSWITCH[2] == 0) begin
 				state = 3;
-				tim = 5;//1000;
+				tim = 1000;
 			end
 		end
 		
@@ -80,9 +122,19 @@ always @(posedge t_out) begin
 			LED = 4'b0011;
 			if(tim == 0) begin
 				state = 4; 
-				tim = 5; //5000;
+				tim = 5000;
 			end 
 			else if(oSWITCH[1] == 0 && oSWITCH[2] == 0) begin
+				if(t2 > t1) begin
+					t_diff = t2 - t1;
+					diff_is_positive = 1;
+					t_diff_state = 0;
+				end
+				else begin
+					t_diff = t1 - t2;
+					diff_is_positive = 0;
+					t_diff_state = 1;
+				end
 				state = 5;
 			end
 		end
@@ -93,6 +145,16 @@ always @(posedge t_out) begin
 				state = 7; 
 			end 
 			else if(oSWITCH[1] == 0 && oSWITCH[2] == 0) begin
+				if(t2 > t1) begin
+					t_diff = t2 - t1;
+					diff_is_positive = 1;
+					t_diff_state = 1;
+				end
+				else begin
+					t_diff = t1 - t2;
+					diff_is_positive = 0;
+					t_diff_state = 1;
+				end
 				state = 5;
 			end
 		end
@@ -106,7 +168,7 @@ always @(posedge t_out) begin
 		
 		6:begin
 			LED = 4'b1011;
-			if(oSWITCH[3] == 0) begin
+			if(oSWITCH[3]) begin
 				state = 7;
 			end
 		end
@@ -122,6 +184,8 @@ always @(posedge t_out) begin
 	if(tim) begin
 		tim = tim - 1;
 	end
+	
+	millis = millis + 1;
 	
 	sim_state = state;
 end
